0e2af2bfe264a7f24dff88709c579150cf4443fb
ChristopherOC <75973858+ChristopherOC@users.noreply.github.com>
2026-07-09 19:03:17 +0200

    adding function_app & updating requirements

---
+++ function_app.py
@@ -0,0 +1,9 @@
+# function_app.py
+import azure.functions as func
+
+from app.main import app as fastapi_app
+
+app = func.AsgiFunctionApp(
+    app=fastapi_app,
+    http_auth_level=func.AuthLevel.ANONYMOUS
+)
